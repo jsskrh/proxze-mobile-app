ebed28c9a4108d17d227aa1f738f5223b84d1ed5
Jesse Akorah <jesse.akorah@sage-grey.com>
2023-06-20 20:22:02 +0100

    Set up screen navigation

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,20 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect, useState, useEffect } from "react";
+y;
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
